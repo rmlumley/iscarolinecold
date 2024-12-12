59c10e3e30692a0364e4dd2dc0bd437f4c7787ce
Rob <rmlumley@gmail.com>
2024-12-12 12:49:47 -0600

    Updated images and dates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,21 @@
 import Alpine from 'alpinejs'
 
+
 window.addEventListener("load", () => {
+	const today = new Date();
+	const returnDate = new Date('2024-12-16');
+	if (today < returnDate) {
+		let city = "cancun,mx";
+		weatherCheck(city);
+	} else {
+		let city = "madison,wi,us";
+		weatherCheck(city);
+	}
 	
+});
+
+function weatherCheck(city) {
 	const api = "e29bb800c350e91c0036cabe600e9786";
-	
-	// const city = "madison,wi,us";
-	const city = "cancun,mx";
-	
-	
 	const base =
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=imperial`;
 	
@@ -16,33 +24,41 @@ window.addEventListener("load", () => {
 	const temperature = document.querySelector("#temperature");
 	const update = document.querySelector("#update");
 	
-	
 	const bg = document.querySelector("#bg");
 	const cold = "/img/cold.jpg";
+	const cool = "/img/cool.jpg";
 	const hot = "/img/hot.jpg";
+	const cloud = "/img/clouds.jpg";
 	
 	fetch(base)
     .then(response => response.json())
     .then(data => {
-      const { main, name, sys, weather } = data;
+      const { main, name, sys, weather, clouds } = data;
 	
 	const currentTemp = Math.round(main.temp);
+	const cloudCover = clouds.all;
 	console.log(currentTemp);
 	
 	  if ((currentTemp) > 72) {
 		update.innerHTML = "<strong>No. It's hot!</strong> Let her roast.";
 		bg.src=hot;
+		if ((cloudCover > 50 )) {
+			update.innerHTML = "<strong>No. It's hot!</strong> Let her roast. <em>But, perhaps it could be less cloudy.</em>";
+			bg.src=cloud;
+		}
 		
 	 } else {
-		update.innerHTML = "<strong>Yes. It's cold!</strong> Get her a blanket.";
-		bg.src=cold;
+		update.innerHTML = "<strong>Yes. It's really cold!</strong> Get her a blanket.";
+		bg.src=cool;
+		if ((currentTemp < 32)) {
+			update.innerHTML = "<strong>Yes. It's really cold!</strong> Get her a <em>heated</em> blanket.";
+			bg.src=cold;
+		}
 	 };
 	 
 	 temperature.textContent = currentTemp;
     })
-	
-	
-});
+}
 
 window.Alpine = Alpine
 
